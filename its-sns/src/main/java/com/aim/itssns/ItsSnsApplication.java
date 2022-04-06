a4892fe1af2629112a46f8c4e6cd02c0b7c103b5
Seonghyun, Bae <xfile6912@u.sogang.ac.kr>
2022-04-06 20:57:32 +0900

    create intellij project

---
+++ its-sns/src/main/java/com/aim/itssns/ItsSnsApplication.java
@@ -0,0 +1,13 @@
+package com.aim.itssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsSnsApplication.class, args);
+    }
+
+}
---
+++ its-sns/src/test/java/com/aim/itssns/ItsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.itssns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
